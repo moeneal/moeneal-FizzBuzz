320cd1c8bb8a20418232e9d24b834391a21a3972
moeneal <momo.mneal@gmail.com>
2023-02-03 04:42:57 +0000

    completed fizz buzz challenge

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -15,6 +15,25 @@ public class FizzBuzz {
      * divisible by both, return 'FizzBuzz'. If none of these are true, return a blank string "".
      */
     public String FizzBuzzGen(int n){
-        return "";
+
+        String fizzOrBuzz = "";
+
+        if(n%3 == 0 ){
+            fizzOrBuzz = "Fizz";
+            if(n%5 == 0){
+                fizzOrBuzz = "FizzBuzz";
+            }
+        }
+        else if( n%5 == 0){
+            fizzOrBuzz = "Buzz";
+
+            if(n%3 == 0){
+                fizzOrBuzz = "FizzBuzz";
+            }
+        }
+        else{
+            fizzOrBuzz = "";
+        }
+        return fizzOrBuzz;
     }
 }
